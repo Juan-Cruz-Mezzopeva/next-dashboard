a49ca04709ffc4e8ad82c30f0c2d8ce0061a06ca
Juan Cruz <mezzopevajuancruz@gmail.com>
2025-04-09 18:06:51 -0300

    carga de fuentes al proyecto en fonts.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from "@/app/ui/home.module.css"
+import { bebas } from './ui/fonts';
 
 const Home = () => {
     return (
         <div>
-            <h1 className={styles.text}>comenzando la app!!</h1>   
+            <h1 className={`${bebas.className} ${styles.text}`}>comenzando la app!!</h1>   
+            <p className='p-10 text-white'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eaque minus pariatur molestiae, est praesentium alias? Rerum, libero tempore exercitationem harum numquam odio sunt, magni repellat, expedita quae delectus officia!</p>
+
         </div>
     );
 };
