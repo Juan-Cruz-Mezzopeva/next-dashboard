f7a448cf91c7ef6fd686d9aea3392c6c877094a0
Juan Cruz <mezzopevajuancruz@gmail.com>
2025-04-18 02:06:57 -0300

    ultimo se agregan las request

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import { FC, PropsWithChildren } from 'react'; 
 import "@/app/ui/globals.css"
 import {roboto} from "./ui/fonts"
+
 const RootLayout: FC<PropsWithChildren> = ({children}) => {
+// children es todo el contenido que esta adentro del layout
     return (
     <html>
         <body className={`${roboto.className} antialiased`}>
